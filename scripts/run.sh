9f0315f0a5aa8cbc2a6a125c29bf1b031c2db0a9
Mark Smith <msmith4@gmail.com>
2023-07-28 15:54:46 +0000

    cpu/memory limits on container

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --rm -it -v chatdocs-db:/app/db -v chatdocs-cache:/root/.cache --name chatdocs p0rt23/chatdocs
+docker run --rm -it \
+	-v chatdocs-db:/app/db \
+	-v chatdocs-cache:/root/.cache \
+	--cpus=4 \
+	--memory=32g \
+	--name chatdocs \
+	p0rt23/chatdocs
